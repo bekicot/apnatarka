6da51d60040bf24f75e83b1d1c619ad29fcb84b8
sublime mind <mindsublimes@gmail.com>
2020-05-10 23:44:23 +0500

    minor update

--- db/migrate/20200508100739_add_column_in_assign_item.rb
+++ db/migrate/20200508100739_add_column_in_assign_item.rb
@@ -1,5 +1,5 @@
 class AddColumnInAssignItem < ActiveRecord::Migration[5.1]
   def change
-  	add_column :assign_items, :status, :integer, default: 0
+  	# add_column :assign_items, :status, :integer, default: 0
   end
 end
